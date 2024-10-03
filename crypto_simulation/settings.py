97ad96eeb550a98a91910b35fcbae7ae1bc332e4
Ama38 <128802670+Ama38@users.noreply.github.com>
2024-10-03 16:57:35 +0500

    alpha build heroku

--- crypto_simulation/settings.py
+++ crypto_simulation/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['crypto-site-uzb-82e6535214cc.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 
